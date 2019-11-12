d3177225c233dc405972ace660805f5380c5f480
Gilgamesh69 logancollier04@gmail.com <Gilgamesh69 logancollier04@gmail.com>
2019-11-11 18:27:32 -0600

    fef

--- src/assignment3/Driver.java
+++ src/assignment3/Driver.java
@@ -1,5 +1,9 @@
 package assignment3;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Driver {
@@ -7,12 +11,22 @@ public class Driver {
 	public static void main(String[] args) {
 		CommandController ctrl = new CommandController();
 		Scanner scnr = new Scanner(System.in);
-		System.out.println("INPUT COMMANDS");
-		System.out.println("TYPE STOP TO STOP");
-		while(true) {
-			if(scnr.nextLine().contentEquals("STOP")) break;
-			ctrl.addCommand(scnr.nextLine());
-		}
+		System.out.println("INPUT FILE");
+		String file = scnr.nextLine();
+		BufferedReader br;
+		try {
+			br = new BufferedReader(new FileReader(file));
+			String line = null;  
+			while ((line = br.readLine()) != null)  {
+				ctrl.addCommand(line);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}  
+		
 		scnr.close();
 	}
 
